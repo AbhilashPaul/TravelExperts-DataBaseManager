4fcbc9fd0b16c6d6545c8d4869700a5095d39c71
Abhilash Paul <paul.abhilash@hotmail.com>
2019-10-21 12:03:04 -0600

    adds invoice ref generator function to invoic class

--- src/travelexperts/models/Invoice.java
+++ src/travelexperts/models/Invoice.java
@@ -1,17 +1,45 @@
 package travelexperts.models;
 
 public class Invoice {
-    private int InvoiceNo;
 
-    public int getInvoiceNo() {
-        return InvoiceNo;
+    private String InvoiceRef;
+
+    public String getInvoiceRef() {
+        InvoiceRef=generateInvoiceRef(6);
+        return InvoiceRef;
     }
 
-    public void setInvoiceNo(int invoiceNo) {
-        InvoiceNo = invoiceNo;
+    public void setInvoiceRef(String invoiceRef) {
+        InvoiceRef = invoiceRef;
     }
 
-    public Invoice(int invoiceNo) {
-        InvoiceNo = invoiceNo;
+    public Invoice(String invoiceRef) {
+        InvoiceRef = invoiceRef;
+    }
+
+    // function to generate a random string of length n
+    public static String generateInvoiceRef(int n)
+    {
+        // chose a Character random from this String
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"+"abcdefghijklmnopqrstuvwxyz";
+
+        // create StringBuffer size of AlphaNumericString
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+
+            // generate a random number between
+            // 0 to AlphaNumericString variable length
+            int index
+                    = (int)(AlphaNumericString.length()
+                    * Math.random());
+
+            // add Character one by one in end of sb
+            sb.append(AlphaNumericString
+                    .charAt(index));
+        }
+
+        return sb.toString();
     }
 }
